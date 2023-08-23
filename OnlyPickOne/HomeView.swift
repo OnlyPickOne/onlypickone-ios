3bd6fd192a5c71da5a5384616b8a395d4370f804
110w110 <110w110.official@gmail.com>
2023-08-23 14:52:11 +0900

    HomeView 구현

---
+++ OnlyPickOne/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  SwiftUITest
+//
+//  Created by 한태희 on 2023/08/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TabView {
+            ListView()
+                .tabItem {
+                    Label("게임목록", systemImage: "gamecontroller.fill")
+                }
+            AddView()
+                .tabItem {
+                    Label("게임 만들기", systemImage: "plus")
+                }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
