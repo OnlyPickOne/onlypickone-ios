e2a48657c58940c7d8cd5ca3bcbef92a069206e3
110w110 <110w110.official@gmail.com>
2023-08-23 20:42:50 +0900

    Game 모델 추가

---
+++ OnlyPickOne/Model/Game.swift
@@ -0,0 +1,29 @@
+//
+//  Game.swift
+//  OnlyPickOne
+//
+//  Created by 한태희 on 2023/08/23.
+//
+
+import Foundation
+
+struct Game: Codable {
+    let id: Int?
+    let title: String?
+    let description: String?
+    let items: [Item]?
+
+//    enum CodingKeys: String, CodingKey {
+//        case id
+//        case caption
+//        case image
+//    }
+//
+//    init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        id = try values.decodeIfPresent(Int.self, forKey: .id)
+//        caption = try values.decodeIfPresent(String.self, forKey: .caption)
+//        image = try values.decodeIfPresent(String.self, forKey: .image)
+//    }
+
+}
