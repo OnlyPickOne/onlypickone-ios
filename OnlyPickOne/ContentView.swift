2d6be6d2d9597c5969eb40d67b44edbdb206e2fd
110w110 <110w110.official@gmail.com>
2023-08-23 14:34:44 +0900

    프로젝트 생성

---
+++ OnlyPickOne/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OnlyPickOne
+//
+//  Created by 한태희 on 2023/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnlyPickOne/OnlyPickOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyPickOneApp.swift
+//  OnlyPickOne
+//
+//  Created by 한태희 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct OnlyPickOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
