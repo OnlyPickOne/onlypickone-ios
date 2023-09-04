2884cf8dac6a6601460081567047bd557679ef4a
110w110 <110w110.official@gmail.com>
2023-09-04 15:00:28 +0900

    design: AddView를 3개의 네비게이션 플로우로 변경

--- OnlyPickOne/View/AddView.swift
+++ OnlyPickOne/View/AddView.swift
@@ -12,31 +12,46 @@ struct AddView: View {
     @State var detailInput: String = ""
     @State var images: [String] = ["cat1","cat2","football"]
     @State var itemInput: [String] = ["","",""]
-    
+    @FocusState private var focusField: Field?
+
     let colors:[Color] = [.purple, .pink, .orange]
     var body: some View {
-        VStack(spacing: 5) {
-            Text("제목")
-            TextField("게임 제목을 작성해주세요", text: $titleInput)
-                .textFieldStyle(.roundedBorder)
-                .padding(15)
-            
-            Text("설명")
-            HStack {
-                TextEditor(text: $detailInput)
-                    .background(Color.primary.colorInvert())
-                    .cornerRadius(5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(.black, lineWidth: 1 / 3)
-                            .opacity(0.3)
-                    )
+        NavigationView {
+            VStack {
+                TextField("게임 제목을 작성해주세요", text: $titleInput)
+                    .focused($focusField, equals: .title)
+                    .textFieldStyle(.roundedBorder)
                     .padding(15)
+                NavigationLink("다음", destination: {
+                    VStack(spacing: 5) {
+                        HStack {
+                            TextEditor(text: $detailInput)
+                                .focused($focusField, equals: .detail)
+                                .background(Color.primary.colorInvert())
+                                .cornerRadius(5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color(UIColor.label), lineWidth: 0.3)
+                                        .opacity(0.3)
+                                )
+                                .padding(15)
+                        }
+                        NavigationLink("다음", destination: {
+                            CardView(input: itemInput, images: images)
+                                .navigationTitle("캡션 달기")
+                        })
+                        .navigationTitle("설명")
+                    }
+                })
+                Spacer()
             }
-            .frame(height: 180)
-            
-            CardView(input: itemInput, images: images)
+            .navigationTitle("제목")
+            .navigationBarTitleDisplayMode(.inline)
         }
+    }
+    
+    enum Field: Hashable {
+        case title, detail
     }
 }
 
