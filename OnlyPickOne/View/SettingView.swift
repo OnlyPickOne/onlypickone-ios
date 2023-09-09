e1eb7cd68bc7ee44d200724748b01bfec8ec66be
110w110 <110w110.official@gmail.com>
2023-09-09 16:15:13 +0900

    fix: SettingView의 Navigation Bar가 안나타나는 이슈 대응

--- OnlyPickOne/View/SettingView.swift
+++ OnlyPickOne/View/SettingView.swift
@@ -9,23 +9,28 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        List {
-            Section("고객센터") {
-                Text("공지사항")
-                Text("문의하기")
-                Text("앱 정보")
+        NavigationView {
+            List {
+                Section("고객센터") {
+                    Text("공지사항")
+                    Text("문의하기")
+                    Text("앱 정보")
+                }
+                
+                Section("개인 설정") {
+                    Text("내 게임")
+                    Text("로그아웃")
+                    Text("회원 탈퇴")
+                }
+                
+                Section("관리자 메뉴 - 관리자만 보일 예정") {
+                    Text("공지 작성")
+                    Text("게임 목록")
+                }
             }
-            
-            Section("개인 설정") {
-                Text("내 게임")
-                Text("로그아웃")
-                Text("회원 탈퇴")
-            }
-            
-            Section("관리자 메뉴 - 관리자만 보일 예정") {
-                Text("공지 작성")
-                Text("게임 목록")
-            }
+            .navigationTitle("OnlyPickOne")
+            .navigationBarTitleDisplayMode(.inline)
+            .tint(Color("opoPink"))
         }
     }
 }
