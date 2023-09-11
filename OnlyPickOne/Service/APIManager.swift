a6c6d7de315ef6f42e08c4bcae6b542a3d1e6b13
110w110 <110w110.official@gmail.com>
2023-09-11 16:17:27 +0900

    feature: Moya 네트워크 모듈 기본 설계

--- OnlyPickOne/Service/APIManager.swift
+++ OnlyPickOne/Service/APIManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Moya
+
+struct APIManager {
+    private let provider = MoyaProvider<APIService>()
+    
+    func test(data: Data, completion: @escaping (String) -> (), failure: @escaping (String) -> ()) {
+        provider.request(.test) { (result) in
+            switch result {
+            case let .success(response):
+                let result = try? response.map(Joke.self)
+                completion(result?.value.joke ?? "")
+            case let .failure(error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
--- OnlyPickOne/Service/APIService.swift
+++ OnlyPickOne/Service/APIService.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+import Moya
+
+enum APIService {
+    case game
+    case gameList
+    case submit
+    case statistics
+    case create
+    case remove
+    case report
+    case join
+    case login
+    case leave
+    case info
+    case notice
+    case test
+}
+
+extension APIService: TargetType {
+    var baseURL: URL {
+        switch self {
+        case .game, .gameList:
+            return URL(string: "http://52.78.136.228")!
+        case .test:
+            return URL(string: "https://api.icndb.com")!
+        default:
+            return URL(string: "http://52.78.136.228")!
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .test:
+            return "/jokes/random"
+        default:
+            return ""
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .test:
+            return .get
+        default:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .test:
+            let params: [String: Any] = [
+                "firstName": "Taehee",
+                "lastName": "Han"
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
+}
