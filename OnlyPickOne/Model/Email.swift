7bf010f5bd4b7087fa93e154735231d4b36aeba0
110w110 <110w110.official@gmail.com>
2023-09-15 13:58:54 +0900

    feat: Email 모델 추가

---
+++ OnlyPickOne/Model/Email.swift
@@ -0,0 +1,12 @@
+//
+//  Email.swift
+//  OnlyPickOne
+//
+//  Created by 한태희 on 2023/09/18.
+//
+
+import Foundation
+
+struct Email: Codable {
+    let email: String?
+}
---
+++ OnlyPickOne/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  OnlyPickOne
+//
+//  Created by 한태희 on 2023/09/18.
+//
+
+import Foundation
