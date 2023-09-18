e15b1a83f5e1e042098fd82a43cd5dc0174beb59
110w110 <110w110.official@gmail.com>
2023-09-18 14:01:31 +0900

    feat: HomeViewModel 구현

--- OnlyPickOne/ViewModel/HomeViewModel.swift
+++ OnlyPickOne/ViewModel/HomeViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+import Moya
+import CombineMoya
+
+class HomeViewModel: ObservableObject {
+    private var subscription = Set<AnyCancellable>()
+    private let provider = MoyaProvider<APIService>()
+    
+    @Published var isAuthorized: Bool = false
+    
+    public func mailAuthReqeust(email: String?) {
+        guard let email = email else { return }
+        provider.requestPublisher(.mailAuthReq(Email(email: email)))
+            .sink { completion in
+                switch completion {
+                case let .failure(error):
+                    print("error: \(error)")
+                case .finished:
+                    print("sucess")
+                }
+            } receiveValue: { response in
+                let result = try? response.map(Response<String>.self)
+                print(result?.message ?? "")
+            }.store(in: &subscription)
+    }
+    
+    init(subscription: Set<AnyCancellable> = Set<AnyCancellable>(), isAuthorized: Bool = false) {
+        self.subscription = subscription
+        self.isAuthorized = isAuthorized
+//        self.mailAuthReqeust(email: "110w110@naver.com")
+    }
+}
