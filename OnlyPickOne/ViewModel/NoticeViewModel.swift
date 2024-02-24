d4e40ffc1d87329afaab48c88d5d6cf98c0296de
110w110 <110w110.official@gmail.com>
2024-02-24 21:02:27 +0900

    feat: NoticeViewModel 구현

--- OnlyPickOne/ViewModel/NoticeViewModel.swift
+++ OnlyPickOne/ViewModel/NoticeViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Combine
+import Moya
+import CombineMoya
+
+class NoticeViewModel: ObservableObject {
+    private var subscription = Set<AnyCancellable>()
+    private let provider = MoyaProvider<APIService>(session: Session(interceptor: AuthInterceptor.shared))
+    
+    @Published var notice: Notice = Notice(noticeId: 0, title: "Test", content: "test", viewCount: 0, createdAt: "2099-01-01T00:00:00")
+    
+    var noticeId: Int
+    
+    func fetchNotice(id: Int) {
+        provider.requestPublisher(.notice(noticeId: id))
+            .sink { completion in
+                switch completion {
+                case let .failure(error):
+                    print("error: \(error)")
+                case .finished:
+                    print("request finished")
+                }
+            } receiveValue: { response in
+                guard let result = try? response.map(Response<Notice>.self), let data = result.data else { return }
+                self.notice = data
+            }.store(in: &subscription)
+    }
+    
+    init(noticeId: Int) {
+        self.noticeId = noticeId
+        self.fetchNotice(id: noticeId)
+    }
+}
+
