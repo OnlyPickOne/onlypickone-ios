16c62cabbbfd7e2aa4e294dba866b32789c4a3e3
110w110 <110w110.official@gmail.com>
2024-02-26 22:05:37 +0900

    fix: 서버 타임존 변경으로 인한 대응

--- OnlyPickOne/Extension/String+.swift
+++ OnlyPickOne/Extension/String+.swift
@@ -11,8 +11,8 @@ extension String {
     func toLastTimeString() -> String {
         let format = DateFormatter()
         format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        format.timeZone = TimeZone(abbreviation: "UTC")
         guard let startTime = format.date(from: self), let endTime = format.date(from: format.string(from: Date())) else {return ""}
+        print("\(startTime) \(endTime) \(Int(endTime.timeIntervalSince(startTime)))")
         return Int(endTime.timeIntervalSince(startTime)).toLastTimeString()
     }
 }
